e712d4fd3b09eb40981c393814f3bdaa6c119bd8
Paulo Venones <paulovenones@MacBook-Pro-de-Paulo.local>
2021-05-06 03:08:07 -0300

    config dynamic environment variables

---
+++ src/pages/api/teste.ts
@@ -0,0 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  return res.status(200).json({ id: 1, name: "iPhone 12" });
+};
--- src/pages/produto/[productId].tsx
+++ src/pages/produto/[productId].tsx
@@ -1,5 +1,5 @@
-import axios from "axios";
 import { GetStaticPaths, GetStaticProps } from "next";
+import axios from "../../axios";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
@@ -11,11 +11,9 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async (context) => {
   const productId = context.params.productId;
 
-  const response = await axios.post(
-    `http://localhost:3000/api/getProductById/${productId}`
-  );
+  const response = await axios.get(`api/getProductById/${productId}`);
 
-  const productData = response.data;
+  const productData = await response.data;
 
   console.log(productData);
 
@@ -29,7 +27,9 @@ export const getStaticProps: GetStaticProps = async (context) => {
 export default function Produto({ product }) {
   return (
     <div>
-      <h1>Produto {product.name}</h1>
+      <h1>
+        Produto {product.name} - id: {product.id}
+      </h1>
     </div>
   );
 }
